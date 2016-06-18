87f7963eb18b5bb0a917fdfcac82126e087855ce
Kurt Wilson <kurt4wilson@gmail.com>
2016-06-18 19:29:33 -0400

    fixed urls

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^scoutingapp/', include('scoutingapp.urls', namespace="scoutingapp")),
+    url(r'^', include('scoutingapp.urls', namespace="scoutingapp")),
     url(r'^admin/', admin.site.urls),
 ]
