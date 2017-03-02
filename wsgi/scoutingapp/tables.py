0ab86903da353244e3e8397b7a901560d3253b9c
Kurt Wilson <kurt4wilson@gmail.com>
2017-03-02 17:21:58 -0500

    order defined

--- wsgi/scoutingapp/tables.py
+++ wsgi/scoutingapp/tables.py
@@ -16,6 +16,7 @@ class MatchTable(tables.Table):
         # add class="bordered"
         attrs = {'class': 'bordered responsive-table'}  # materialze class
         exclude = ('tournament', 'score', 'id')
+        sequence = ('match_number', 'scouted_team', 'auto_gears_scored', 'auto_move_yn', 'auto_hoppers_triggered', 'auto_hopper_load', 'auto_high_goal_accuracy', 'auto_low_goal_accuracy', 'teleop_hoppers_triggered', 'robot_end_game', 'robot_card', 'scouted_by', 'duplicate')
 
 class VolleyTable(tables.Table):
 
