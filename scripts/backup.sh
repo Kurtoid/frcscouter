09a9d46754df5bb7cff828e9eb24d8929049df51
Kurt Wilson <Kurt4wilson@gmail.com>
2017-03-16 19:27:48 -0400

    daily backups

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,11 +4,13 @@
 #Author = Hafiz Haider
 #Version 1.0
 #START
-echo "$PWD"
 TIME=`date +%b-%d-%y`            # This Command will add date in Backup File Name.
 FILENAME=backup-$TIME.tar.gz    # Here i define Backup file name format.
-SRCDIR="$(dirname $PWD)/wsgi/scouter"                  # Location of Important Data Directory (Source of backup).
-DESDIR="$(dirname $PWD)/backups"            # Destination of backup file.
+#THISDIR=$(dirname $PWD)
+if [ -z ${OPENSHIFT_REPO_DIR+x} ]; then THISDIR=$(dirname $PWD); else THISDIR=$OPENSHIFT_REPO_DIR; fi
+echo $THISDIR
+SRCDIR=$THISDIR"/wsgi/scouter"                  # Location of Important Data Directory (Source of backup).
+DESDIR=$THISDIR"/backups"            # Destination of backup file.
 echo $SRCDIR
 echo tar -cpzf "$DESDIR/$FILENAME" "$SRCDIR"
 tar -cpzf "$DESDIR/$FILENAME" "$SRCDIR"
